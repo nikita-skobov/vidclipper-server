7852f209cd00ae60e07e66a1236f07aca85fae5b
nikita-skobov <nskobov.websites@gmail.com>
2020-12-08 23:07:26 -0600

    adds transcode stage

--- src/download_manager.rs
+++ src/download_manager.rs
@@ -33,6 +33,7 @@ pub struct DownloadRequest {
     pub name: Option<String>,
     pub start: Option<u32>,
     pub duration: Option<u32>,
+    pub transcode_extension: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -40,6 +41,13 @@ pub struct SplitRequest {
     pub start: Option<u32>,
     pub duration: Option<u32>,
     pub output_prefix: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TranscodeRequest {
+    pub transcode_extension: Option<String>,
+    pub clip_name_matching: String,
+    pub duration: Option<u32>,
 }
 
 // TODO: dont iter over all alphanumeric, we only
@@ -353,6 +361,106 @@ pub async fn cut_video(
     handle_child_exit(child_status)
 }
 
+pub async fn transcode_clip(
+    url: String,
+    transcode_request: TranscodeRequest,
+) -> TaskResult {
+    if transcode_request.transcode_extension.is_none() {
+        // no point in running ffmpeg just to copy the existing streams
+        // to a new file.
+        return Ok(());
+    }
+    let extension = transcode_request.transcode_extension.unwrap();
+
+
+    let mut input_path = find_file_path_by_match(&transcode_request.clip_name_matching, ".").await?;
+
+    let input_string = match input_path.to_str() {
+        Some(s) => s.to_string(),
+        None => {
+            let error_string = format!("File path contains invalid characters: {:?}", input_path);
+            return Err(error_string);
+        }
+    };
+    input_path.set_extension(extension);
+    let output_file_name = match input_path.file_name() {
+        None => {
+            let error_string = format!("File path contains invalid characters: {:?}", input_path);
+            return Err(error_string);
+        },
+        Some(os_str) => match os_str.to_str() {
+            Some(s) => s.to_string(),
+            None => {
+                let error_string = format!("File path contains invalid characters: {:?}", os_str);
+                return Err(error_string);
+            }
+        },
+    };
+
+    let mut exe_and_args = vec![
+        "ffmpeg".into(),
+        "-loglevel".into(),
+        "error".into(),
+        "-hide_banner".into(),
+        "-stats".into(),
+        "-progress".into(),
+        "pipe:1".into(),
+    ];
+    exe_and_args.push("-i".into());
+    exe_and_args.push(input_string);
+    exe_and_args.push("-y".into());
+    exe_and_args.push(output_file_name);
+    println!("running with commands:\n{:#?}", exe_and_args);
+    let cmd = create_command(&exe_and_args[..]);
+
+    // create a reader from the stdout handle we created
+    // pass that reader into the following future spawned on tokio
+    let (child, mut reader, _) = setup_child_and_reader(cmd)?;
+
+    let duration_millis = match transcode_request.duration {
+        None => 1, // TODO: find the total duration of the input file via ffprobe
+        Some(d) => d * 1000,
+    };
+    tokio::spawn(async move {
+        loop {
+            let thing = reader.next_line().await;
+            if let Err(e) = thing {
+                println!("there was error: {}", e);
+                break;
+            }
+            let thing = thing.unwrap();
+
+            // break if we didnt get a line, ie: end of line
+            if let None = thing {
+                break;
+            } else if let Some(ref line) = thing {
+                let time_string = get_time_string_from_line(&line);
+                if time_string.is_none() {
+                    continue;
+                }
+                let time_millis = get_millis_from_time_string(time_string.unwrap());
+                if time_millis.is_none() {
+                    continue;
+                }
+                let time_millis = time_millis.unwrap();
+                let progress = time_millis as f64 / duration_millis as f64;
+                println!("time_millis: {}, duration_millis: {}, progress: {}", time_millis, duration_millis, progress);
+                use_me_from_progress_holder(&url, &PROGHOLDER, |me| {
+                    me.inc_progress_percent_normalized(progress);
+                });
+            }
+        }
+    });
+
+    // the above happens asynchronously, but here we await the child process
+    // itself. as we await this child process, the above async future can run
+    // whenever the reader finds a next line. But after here we actually return
+    // our TaskResult that is read by the progresslib2
+    let child_status = child.await;
+
+    handle_child_exit(child_status)
+}
+
 pub async fn find_file_path_by_match<S: AsRef<str>, P: AsRef<Path>>(
     matching: S,
     path: P,
@@ -411,12 +519,22 @@ pub fn create_download_item(
         Ok(guard) => guard.contains_key(&url),
     };
 
+    let output_clip_prefix = format!("clipped.{}.", name.clone());
+    let transcode_stage = make_stage!(transcode_clip;
+        url.clone(),
+        TranscodeRequest {
+            transcode_extension: download_request.transcode_extension,
+            clip_name_matching: output_clip_prefix.clone(),
+            duration: download_request.duration,
+        }
+    );
+
     let cut_stage = make_stage!(cut_video;
         url.clone(),
         SplitRequest {
             start: download_request.start,
             duration: download_request.duration,
-            output_prefix: format!("clipped.{}.", name.clone()),
+            output_prefix: output_clip_prefix,
         }
     );
 
@@ -432,6 +550,7 @@ pub fn create_download_item(
     // the video yet. in that case, it must happen
     // BEFORE the cut stage... obviously
     progitem.register_stage(cut_stage);
+    progitem.register_stage(transcode_stage);
     progitem
 }
 
