74b1d508f5bb879b9da88908fd232c5a54934354
nikita-skobov <nskobov.websites@gmail.com>
2020-12-12 20:58:54 -0600

    adds a Config struct to data_store. uses generic methods for data_store loading

--- src/data_store.rs
+++ src/data_store.rs
@@ -1,8 +1,12 @@
 use std::path::Path;
 use std::path::PathBuf;
 use std::{fmt::Display, collections::HashMap};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub download_dir: PathBuf,
+}
 
 #[derive(Clone, Default, Debug, Serialize, Deserialize)]
 pub struct DownloadedVideo {
@@ -32,17 +36,19 @@ pub fn string_error(e: impl Display) -> String {
     format!("{}", e)
 }
 
-pub fn read_json_data<P: AsRef<Path>>(path: P) -> Result<DownloadedVideos, String> {
+pub fn read_json_data<T: DeserializeOwned, P: AsRef<Path>>(path: P) -> Result<T, String> {
     let json_string = std::fs::read_to_string(path).map_err(string_error)?;
     json_string_to_data(json_string)
 }
 
-pub fn json_string_to_data<S: AsRef<str>>(json_string: S) -> Result<DownloadedVideos, String> {
-    let vid_map: DownloadedVideos = serde_json::from_str(
+pub fn json_string_to_data<T: DeserializeOwned, S: AsRef<str>>(
+    json_string: S
+) -> Result<T, String> {
+    let obj: T = serde_json::from_str(
         json_string.as_ref()
     ).map_err(string_error)?;
 
-    Ok(vid_map)
+    Ok(obj)
 }
 
 pub fn data_to_json_string(data: &DownloadedVideos) -> Result<String, String> {
@@ -54,7 +60,7 @@ pub fn write_json_data<P: AsRef<Path>>(path: P, data: &DownloadedVideos) -> Resu
     std::fs::write(path, json_string).map_err(string_error)
 }
 
-pub fn initialize_data<P: AsRef<Path>>(path: P) -> Result<DownloadedVideos, String> {
+pub fn initialize_object<T: DeserializeOwned, P: AsRef<Path>>(path: P) -> Result<T, String> {
     if !path.as_ref().exists() {
         // create it as an empty json file if this
         // is the first time initializing
@@ -62,6 +68,14 @@ pub fn initialize_data<P: AsRef<Path>>(path: P) -> Result<DownloadedVideos, Stri
     }
 
     read_json_data(path)
+}
+
+pub fn initialize_data<P: AsRef<Path>>(path: P) -> Result<DownloadedVideos, String> {
+    initialize_object(path)
+}
+
+pub fn initialize_config<P: AsRef<Path>>(path: P) -> Result<Config, String> {
+    initialize_object(path)
 }
 
 #[cfg(test)]
@@ -77,7 +91,7 @@ mod tests {
         }
         "#;
 
-        let data = json_string_to_data(json_string).unwrap();
+        let data: DownloadedVideos = json_string_to_data(json_string).unwrap();
         let data_map = data.as_ref();
         assert!(data_map.contains_key("a"));
         assert!(data_map.contains_key("b"));
